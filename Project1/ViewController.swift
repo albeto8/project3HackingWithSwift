4a6c916b3b05c4994088783fac1b420f61e03a8d
Mario Alberto Barragán Espinosa <>
2019-09-15 18:35:13 -0500

    Day 16 Setting up

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Mario Alberto Barragán Espinosa on 9/15/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
